2d5e62c0c2283b514689ac890a8ddcb8d4f67b12
jcruzram <jcruzram@gmail.com>
2023-09-27 21:30:11 -0700

    delete commented code

--- whitelabel/monkey_patches/frappe.py
+++ whitelabel/monkey_patches/frappe.py
@@ -1,29 +1,4 @@
 import frappe
-# import functools
-# import io
-# import itertools
-# import json
-# import operator
-# import os
-# import re
-# from contextlib import contextmanager
-# from csv import reader
-
-# from frappe.utils import cstr, get_bench_path, is_html, strip, strip_html_tags, unique
-# from frappe.translate import get_translations_from_apps, get_user_translations
-# # Cache keys
-# MERGED_TRANSLATION_KEY = "merged_translations"
-# USER_TRANSLATION_KEY = "lang_user_translations"
-
-# from typing import TYPE_CHECKING, Any, Callable, Literal, Optional, overload
-
-# import click
-# from werkzeug.local import Local, release_local
-
-# from frappe import as_unicode
-
-# controllers = {}
-# local = Local()
 
 old_ = frappe._
 
